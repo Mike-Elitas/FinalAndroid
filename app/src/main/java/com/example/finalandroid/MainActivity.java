5a82353fbeb93bbff912a5bd36a34b61cfa74b47
Ivan Bosch <ivan.bosch.amengual@gmail.com>
2022-03-10 18:20:16 +0100

    v1.0 FIX

--- app/src/main/java/com/example/finalandroid/MainActivity.java
+++ app/src/main/java/com/example/finalandroid/MainActivity.java
@@ -151,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
         char result = checkLines();
         if (result != 'n') {
             if (result == 't') {
+                playerWin = true;
                 sound = MediaPlayer.create(this, R.raw.win);
                 sound.start();
                 imagePop(R.drawable.tie);
@@ -197,90 +198,147 @@ public class MainActivity extends AppCompatActivity {
         sound.start();
     }
 
-    private char checkForWinningMove(char a, char b, char c){
-        if ( a == b && a != c && c == 0 && a == (char) -1)
-            return 2;
-        else if (a == c && a != b && b == 0 && a == (char) -1)
-            return 1;
-        else if (b == c && b != a && a == 0 && b == (char) -1)
-            return 0;
-        else if ( a == b && a != c && c == 0)
-            return 2;
-        else if (a == c && a != b && b == 0)
-            return 1;
-        else if (b == c && b != a && a == 0)
-            return 0;
-        else return 'n';
+    private boolean canWin(char a, char b, char c){
+        if ( a == b && c == 0 && a == (char) -1)
+            return true;
+        else if (a == c && b == 0 && a == (char) -1)
+            return true;
+        else return b == c && a == 0 && b == (char) -1;
+    }
+
+    private boolean canLose(char a, char b, char c){
+        if (a == b && c == 0 && a == 1)
+            return true;
+        else if (a == c && b == 0 && a == 1)
+            return true;
+        else return b == c && a == 0 && b == 1;
     }
 
     private void hardMove(){
         boolean defendAtack = false;
-        for (int i = 0; i < 3; i++) {
-            if (checkForWinningMove(board[i][0], board[i][1], board[i][2]) != 'n') {
-                if (checkForWinningMove(board[i][0], board[i][1], board[i][2]) == 0){
-                    board[i][0] = (char) -1;
-                    imageButtons[i][0].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
-                } else if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) == 1){
-                    board[i][1] = (char) -1;
-                    imageButtons[i][1].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
-                } else {
-                    board[i][2] = (char) -1;
-                    imageButtons[i][2].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
-                }
-            }
-        }
 
         for (int i = 0; i < 3; i++) {
-            if (checkForWinningMove(board[0][i], board[1][i], board[2][i]) != 'n') {
-                if (checkForWinningMove(board[0][i], board[1][i], board[2][i]) == 0){
-                    board[0][i] = (char) -1;
+            if (!defendAtack && (canWin(board[i][0], board[i][1], board[i][2]))){
+                for (int j = 0; j < 3; j++) {
+                    if (imageButtons[i][j].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                        imageButtons[i][j].setBackgroundResource(R.drawable.o);
+                        board[i][j] = (char) -1;
+                        break;
+                    }
+                }
+                defendAtack = true;
+            }
+
+            if (!defendAtack && (canWin(board[0][i], board[1][i], board[2][i]))){
+                if (imageButtons[0][i].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                     imageButtons[0][i].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
-                } else if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) == 1){
-                    board[1][i] = (char) -1;
-                    imageButtons[1][i].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
-                } else {
-                    board[2][i] = (char) -1;
-                    imageButtons[2][i].setBackgroundResource(R.drawable.o);
-                    defendAtack = true;
+                    board[0][i] = (char) -1;
                 }
+                if (imageButtons[1][i].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()) {
+                    imageButtons[1][i].setBackgroundResource(R.drawable.o);
+                    board[1][i] = (char) -1;
+                }
+                if (imageButtons[2][i].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[2][i].setBackgroundResource(R.drawable.o);
+                    board[2][i] = (char) -1;
+                }
+                defendAtack = true;
             }
         }
 
-        if (checkForWinningMove(board[0][0], board[1][1], board[2][2]) != 'n') {
-            if (checkForWinningMove(board[0][0], board[1][1], board[2][2]) == 0){
-                board[0][0] =(char) -1;
+        if (!defendAtack && canWin(board[0][0], board[1][1], board[2][2])){
+            if (imageButtons[0][0].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[0][0].setBackgroundResource(R.drawable.o);
-                defendAtack = true;
-            } else if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) == 1){
-                board[1][1] =(char) -1;
+                board[0][0] = (char) -1;
+            }
+            if (imageButtons[1][1].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[1][1].setBackgroundResource(R.drawable.o);
-                defendAtack = true;
-            } else {
-                board[2][2] =(char) -1;
+                board[1][1] = (char) -1;
+            }
+            if (imageButtons[2][2].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[2][2].setBackgroundResource(R.drawable.o);
-                defendAtack = true;
+                board[2][2] = (char) -1;
             }
+            defendAtack = true;
         }
-        if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) != 'n') {
-            if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) == 0){
-                board[2][0] =(char) -1;
+
+        if (!defendAtack && canWin(board[2][0], board[1][1], board[0][2])){
+            if (imageButtons[2][0].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[2][0].setBackgroundResource(R.drawable.o);
-                defendAtack = true;
-            } else if (checkForWinningMove(board[2][0], board[1][1], board[0][2]) == 1){
-                board[1][1] =(char) -1;
+                board[2][0] = (char) -1;
+            }
+            if (imageButtons[1][1].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[1][1].setBackgroundResource(R.drawable.o);
-                defendAtack = true;
-            } else {
-                board[0][2] =(char) -1;
+                board[1][1] = (char) -1;
+            }
+            if (imageButtons[0][2].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
                 imageButtons[0][2].setBackgroundResource(R.drawable.o);
+                board[0][2] = (char) -1;
+            }
+            defendAtack = true;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (!defendAtack && (canLose(board[j][0], board[j][1], board[j][2]))){
+                for (int h = 0; h < 3; h++) {
+                    if (imageButtons[j][h].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()) {
+                        imageButtons[j][h].setBackgroundResource(R.drawable.o);
+                        board[j][h] = (char) -1;
+                        break;
+                    }
+                }
+                defendAtack = true;
+            }
+
+            if (!defendAtack && (canLose(board[0][j], board[1][j], board[2][j]))){
+                if (imageButtons[0][j].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[0][j].setBackgroundResource(R.drawable.o);
+                    board[0][j] = (char) -1;
+                }
+                if (imageButtons[1][j].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()) {
+                    imageButtons[1][j].setBackgroundResource(R.drawable.o);
+                    board[1][j] = (char) -1;
+                }
+                if (imageButtons[2][j].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[2][j].setBackgroundResource(R.drawable.o);
+                    board[2][j] = (char) -1;
+                }
+                defendAtack = true;
+            }
+
+            if (!defendAtack && canLose(board[0][0], board[1][1], board[2][2])){
+                if (imageButtons[0][0].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[0][0].setBackgroundResource(R.drawable.o);
+                    board[0][0] = (char) -1;
+                }
+                if (imageButtons[1][1].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[1][1].setBackgroundResource(R.drawable.o);
+                    board[1][1] = (char) -1;
+                }
+                if (imageButtons[2][2].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[2][2].setBackgroundResource(R.drawable.o);
+                    board[2][2] = (char) -1;
+                }
+                defendAtack = true;
+            }
+
+            if (!defendAtack && canLose(board[2][0], board[1][1], board[0][2])){
+                if (imageButtons[2][0].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[2][0].setBackgroundResource(R.drawable.o);
+                    board[2][0] = (char) -1;
+                }
+                if (imageButtons[1][1].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[1][1].setBackgroundResource(R.drawable.o);
+                    board[1][1] = (char) -1;
+                }
+                if (imageButtons[0][2].getBackground().getConstantState() == getDrawable(R.drawable.blank).getConstantState()){
+                    imageButtons[0][2].setBackgroundResource(R.drawable.o);
+                    board[0][2] = (char) -1;
+                }
                 defendAtack = true;
             }
         }
+
         if (!defendAtack) randomMove();
         sound.start();
     }
